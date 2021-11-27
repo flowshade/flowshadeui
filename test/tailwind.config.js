3e1b3fb6335fec2ed0a1c7b38b7288dcece0e34a
rakshitranga <61892635+rakshitranga@users.noreply.github.com>
2021-11-27 15:41:50 -0500

    Co-authored-by: Sameera <SameeraMurthy@users.noreply.github.com>

--- test/tailwind.config.js
+++ test/tailwind.config.js
@@ -2,11 +2,11 @@ module.exports = {
     purge: ["./*.html", "./*.js"],
     darkMode: "class", // or 'media' or 'class'
     theme: {
-        extend: {
-            backgroundImage: theme => ({
-                'select-arrow': "url('../arrows-scroll-v.svg')"
-            })
-        }
+        // extend: {
+        //     backgroundImage: theme => ({
+        //         'select-arrow': "url('../arrows-scroll-v.svg')"
+        //     })
+        // }
     },
     plugins: [
         require("../index")
